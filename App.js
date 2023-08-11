e8c4d897bb334af138df947f3a74500f9d95bac8
Pablo Bassil <pabcubus@gmail.com>
2023-08-11 01:10:25 -0500

    Added new login page

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
-import { StyleSheet } from 'react-native';
-import { useFonts } from 'expo-font';
-import Constants from 'expo-constants';
 import SearchProducts from './pages/SearchProducts/SearchProducts';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [fontsLoaded] = useFonts({
-    'Montserrat': require('./assets/fonts/Montserrat.otf'),
-  });
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
         <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
         <Stack.Screen name="Search" component={SearchProducts} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    fontFamily: 'Montserrat'
-  }
-});
 
 export default App
---
+++ pages/Login/Login.js
@@ -0,0 +1,48 @@
+import { Flex, Button, Text, TextInput } from "@react-native-material/core";
+import { StyleSheet, SafeAreaView } from "react-native";
+import Constants from "expo-constants";
+
+const Login = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Flex direction="column" style={materialStyles.container}>
+        <TextInput label="Usuario" variant="standard" />
+        <TextInput label="Contraseña" variant="standard" />
+        <Button
+          title={() => (
+            <Text style={materialStyles.button}>
+              Login!
+            </Text>
+          )}
+          onPress={() => navigation.navigate("Search")}>
+        </Button>
+      </Flex>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+const materialStyles = {
+  button: {
+    fontFamily: 'Montserrat Regular',
+    color: 'white',
+    fontSize: 15
+  },
+  container: {
+    padding: 10,
+    gap: 40,
+  },
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    fontFamily: 'Montserrat Regular',
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    justifyContent: 'center',
+  },
+  button: {
+    fontFamily: 'Montserrat Regular',
+  }
+});
