3411214715dbbedaa74fc25cb11e64323b7c5abd
Pablo Bassil <pabcubus@gmail.com>
2023-09-02 00:40:40 -0500

    Modified the inputs and put new fonts

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import { Flex, Button, Text, TextInput } from "@react-native-material/core";
 import { StyleSheet, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
+import * as Font from 'expo-font';
 
 const Login = ({ navigation }) => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        'monserrat-regular': require('../../assets/fonts/MontserratRegular.ttf'),
+      });
+
+      setFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return (<Text>Loading...</Text>);
+  }
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Flex direction="column" style={materialStyles.container}>
-        <TextInput label="Usuario" variant="standard" />
+      <Flex
+        direction="column"
+        style={materialStyles.container}>
         <TextInput
-          label="Contraseña"
+          inputStyle={materialStyles.input}
+          style={materialStyles.input}         
+          placeholder="Usuario"
+          variant="standard" />
+        <TextInput
+          inputStyle={materialStyles.input}
+          style={materialStyles.input}          
+          placeholder="Contraseña"
           variant="standard"
           secureTextEntry={true} />
         <Button
@@ -27,8 +55,11 @@ const Login = ({ navigation }) => {
 export default Login;
 
 const materialStyles = {
+  input: {
+    fontFamily: 'monserrat-regular'
+  },
   button: {
-    fontFamily: 'Montserrat Regular',
+    fontFamily: 'monserrat-regular',
     color: 'white',
     fontSize: 15
   },
@@ -40,12 +71,8 @@ const materialStyles = {
 
 const styles = StyleSheet.create({
   safeArea: {
-    fontFamily: 'Montserrat Regular',
     flex: 1,
     paddingTop: Constants.statusBarHeight,
     justifyContent: 'center',
-  },
-  button: {
-    fontFamily: 'Montserrat Regular',
   }
 });
